77ec1194c9ee7986c24450462a158c019d9d029e
Restyled.io <commits@restyled.io>
2024-05-20 06:13:52 +0000

    Restyled by terraform

--- locals.tf
+++ locals.tf
@@ -1,75 +1,75 @@
-locals{
-    project = {
-        "fem-eci-project" = {
-            description = "This is a test project"
-        }
+locals {
+  project = {
+    "fem-eci-project" = {
+      description = "This is a test project"
     }
-    workspace = {
-        "fem-eci-tfe" = {
-            description = "This is a test workspace"
-            execution_mode = "remote"
-            project_id = module.project["fem-eci-project"].id
-            vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
-        }
-        "fem-eci-github" = {
-            description = "Example automation workspace for GitHub resources"
-            execution_mode = "remote"
-            project_id = module.project["fem-eci-project"].id
-            vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
-        }
-        "fem-eci-aws-network" = {
-            description         = "Automation for AWS network resources."
-            execution_mode      = "remote"
-            project_id          = module.project["fem-eci-project"].id
-            vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-aws-network"
-
-            variables = [
-                {
-                category = "terraform"
-                hcl      = true
-                key      = "azs"
-                value    = jsonencode(["us-west-2a", "us-west-2b"])
-                },
-                {
-                category = "terraform"
-                key      = "cidr"
-                value    = "10.0.0.0/16"
-                },
-                {
-                category = "terraform"
-                key      = "name"
-                value    = "fem-eci"
-                },
-            ]
-        }
-        "fem-eci-aws-cluster-prod" = {
-            description         = "Automation for AWS cluster resources."
-            execution_mode      = "remote"
-            project_id          = module.project["fem-eci-project"].id
-            vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-aws-cluster"
-
-            variables = [
-                {
-                category = "terraform"
-                key      = "domain"
-                value    = "fem-eci.com"
-                },
-                {
-                category = "terraform"
-                key      = "environment"
-                value    = "prod"
-                },
-                {
-                category = "terraform"
-                key      = "name"
-                value    = "fem-eci-alejandrogm"
-                },
-                {
-                category = "terraform"
-                key      = "vpc_name"
-                value    = "fem-eci"
-                },
-            ]
-        }
+  }
+  workspace = {
+    "fem-eci-tfe" = {
+      description         = "This is a test workspace"
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
     }
+    "fem-eci-github" = {
+      description         = "Example automation workspace for GitHub resources"
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
+    }
+    "fem-eci-aws-network" = {
+      description         = "Automation for AWS network resources."
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-aws-network"
+
+      variables = [
+        {
+          category = "terraform"
+          hcl      = true
+          key      = "azs"
+          value    = jsonencode(["us-west-2a", "us-west-2b"])
+        },
+        {
+          category = "terraform"
+          key      = "cidr"
+          value    = "10.0.0.0/16"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "fem-eci"
+        },
+      ]
+    }
+    "fem-eci-aws-cluster-prod" = {
+      description         = "Automation for AWS cluster resources."
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-aws-cluster"
+
+      variables = [
+        {
+          category = "terraform"
+          key      = "domain"
+          value    = "fem-eci.com"
+        },
+        {
+          category = "terraform"
+          key      = "environment"
+          value    = "prod"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "fem-eci-alejandrogm"
+        },
+        {
+          category = "terraform"
+          key      = "vpc_name"
+          value    = "fem-eci"
+        },
+      ]
+    }
+  }
 }
